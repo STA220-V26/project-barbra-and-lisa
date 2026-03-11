7f991447c64f8b770a209cb4ba825f5ee49161da
BarbraKizikuOtiende <gusotiba@student.gu.se>
2026-03-11 16:24:58 +0100

    Cleaning the patients data

--- _targets.R
+++ _targets.R
@@ -26,39 +26,7 @@ invisible(lapply(pkgs, library, character.only = TRUE))
 tar_option_set(
   packages = c("tibble"), # Packages that your targets need for their tasks.
   format = "qs" # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-  #   controller = crew::crew_controller_local(workers = 2, seconds_idle = 60)
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  #
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
-  # Set other options as needed.
 )
-
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
 # tar_source("other_functions.R") # Source other scripts as needed.
@@ -81,6 +49,18 @@ list(
   #unzip the file
   tar_target(cvs_files, zip::unzip(zipdata)),
 
+tar_target(
+  patients_clean,
+  {
+    patients <- setDT(patients_raw)
+    setkey(patients, id)
+
+    patients <- janitor::remove_empty(patients, quiet = TRUE)
+    patients <- janitor::remove_constant(patients, quiet = TRUE)
+
+    patients
+  }
+),
   #dynamically read all files found in data-fixed
   #I removed the fs::dir_map line because it was causing the error
   tar_map(
