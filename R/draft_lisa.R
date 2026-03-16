9a2e73f8d0077fcafdc5162a254c8fef6f04400d
Lisa Mag <lisa@skogskar.se>
2026-03-16 13:28:13 +0100

    Commented out draft_Lisa so tar_make() works better

--- R/draft_lisa.R
+++ R/draft_lisa.R
@@ -1,20 +1,20 @@
-library(ggplot2)
-library(caTools)
-library(dplyr)
+#library(ggplot2)
+#library(caTools)
+#library(dplyr)
 
 
-patients <-
-  readr::read_csv(unz("data.zip", "data-fixed/patients.csv")) |>
-  setDT() |>
-  setkey(id)|>
+#patients <-
+#  readr::read_csv(unz("data.zip", "data-fixed/patients.csv")) |>
+#  setDT() |>
+#  setkey(id)|>
 
-patients <- patients |>
-  mutate(age = floor(interval(birthdate, today()) / years(1)))
+#patients <- patients |>
+#  mutate(age = floor(interval(birthdate, today()) / years(1)))
 # mutate is a function from the dpylr package while we want to use functions from the data.table package for consistency and better perfomance.
-patients <- patients |>
-age := floor(lubridate::interval(birthdate, lubridate::today()) / lubridate::years(1))
-plot(patients$age, patients$healthcare_expenses)
+#patients <- patients |>
+#age := floor(lubridate::interval(birthdate, lubridate::today()) / lubridate::years(1))
+#plot(patients$age, patients$healthcare_expenses)
 
 
 
-hist(patients$gender, patients$healthcare_expenses)
+#hist(patients$gender, patients$healthcare_expenses)
